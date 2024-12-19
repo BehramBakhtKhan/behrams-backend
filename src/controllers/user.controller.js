19c45982666bc4cc0f0caac83017555eb21ee162
BehramBakhtKhan <behrambakhtkhan22@gmail.com>
2024-12-19 23:04:51 +0500

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
-import {User} from "../models/user.model.js"
+import { User } from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
@@ -41,7 +41,7 @@ const registerUser = asyncHandler(async(req, res ) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath =  req.files()?.coverImage[0]?.path;
+    const coverImageLocalPath =  req.files?.coverImage[0]?.path;
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
