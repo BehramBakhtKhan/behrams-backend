0bcf2c1e678060806f348ac2a5b9fa3d1e1a3977
BehramBakhtKhan <behrambakhtkhan22@gmail.com>
2025-02-03 21:28:26 +0500

    refactor: standardize import statements and fix casing issues in models; enhance cloudinary utility with delete functionality

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -1,27 +1,29 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose"
 
 const playlistSchema = new Schema(
     {
         name: {
-            type: string,
-            required: true
+            type: String,
+            required: [true, "name is required"]
         },
         description: {
-            type: string,
-            required: true
+            type: String,
+            required: [true, "description is required"]
         },
         videos: [
             {
                 type: Schema.Types.ObjectId,
                 ref: "Video"
-            }
+            } 
         ],
         owner: {
             type: Schema.Types.ObjectId,
-                ref: "User"
-        },
-    },
-    {timestamps: true}
-)
+            ref: "User"
+        }  
+    }, 
+    {
+        timestamps: true
+    }
+);
 
-export const Playlist = mongoose.model("Playlist", playlistSchema)
+export const Playlist = mongoose.model("Playlist", playlistSchema);
