f6d38bc0c304b35e85007458c97a25c5259d21a4
BehramBakhtKhan <behrambakhtkhan22@gmail.com>
2025-02-03 20:27:18 +0500

    feat: refactor video routes to include video management functionalities and JWT verification

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,17 +1,43 @@
-import { Router } from 'express';
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
-    createTweet,
-    deleteTweet,
-    getUserTweets,
-    updateTweet,
-} from "../controllers/tweet.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+    deleteVideo,
+    getAllVideos,
+    getVideoById,
+    updateVideo,
+    publishAVideo,
+    togglePublishStatus
+} from "../controllers/video.controller.js";
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post(createTweet);
-router.route("/user/:userId").get(getUserTweets);
-router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+// router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-export default router
+router
+    .route("/")
+    .get(getAllVideos)
+    .post(
+        verifyJWT,
+        upload.fields([
+            {
+                name: "videoFile",
+                maxCount: 1
+            },
+            {
+                name: "thumbnail",
+                maxCount: 1
+            }
+        ]),
+        publishAVideo
+    );
+
+router
+    .route("/v/:videoId")
+    .get(verifyJWT, getVideoById)
+    .delete(verifyJWT, deleteVideo)
+    .patch(verifyJWT, upload.single("thumbnail"), updateVideo);
+
+router.route("/toggle/publish/:videoId").patch(verifyJWT, togglePublishStatus);
+
+export default router;
